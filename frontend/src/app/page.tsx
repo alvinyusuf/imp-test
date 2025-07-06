d12835e07e882aff1e591752ede1e8c1491741d3
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2025-07-06 22:58:33 +0700

    Refactor layout in Home component for improved spacing and alignment

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,8 @@ import Posts from "./posts/page";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-start px-4 py-8 md:px-0 md:py-4">
-      <div className="flex w-full justify-end px-8">
+    <main className="flex flex-col items-center justify-start px-4 py-8 gap-y-2 md:px-0 md:py-4 md:gap-y-0">
+      <div className="flex w-full justify-start md:justify-end md:px-8">
         <Logout />
       </div>
       <div className="w-full sm:w-4/5 md:w-2/3">
