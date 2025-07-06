9feb8ae6d6607c945696e201a38275a94915213a
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2025-07-06 15:24:13 +0700

    Refactor PostActions component: add confirmation dialog for delete action and improve modal handling

--- frontend/src/app/posts/components/action.tsx
+++ frontend/src/app/posts/components/action.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import { Pencil, Trash } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 
 type PostActionsProps = {
   id: string;
@@ -8,17 +10,36 @@ type PostActionsProps = {
 };
 
 export default function PostActions({ id, onDelete }: PostActionsProps) {
+  const modal = useRef<HTMLDialogElement>(null);
+
   return (
     <div className="flex items-center gap-2">
       <Link href={`/posts/${id}`} className="btn btn-xs btn-warning">
         <Pencil size={16} /> Edit
       </Link>
       <button
-        onClick={(e) => onDelete(e, Number(id))}
         className="btn btn-xs btn-error"
+        onClick={() => modal.current?.showModal()}
       >
         <Trash size={16} /> Delete
       </button>
+
+      <dialog ref={modal} className="modal">
+        <div className="modal-box text-center md:w-1/3">
+          <h3 className="font-bold text-lg">Delete Post?</h3>
+          <div className="modal-action">
+            <form method="dialog" className="w-full flex justify-between items-center">
+              <button className="btn btn-neutral w-1/3">Cancel</button>
+              <button
+                onClick={(e) => onDelete(e, Number(id))}
+                className="btn btn-error w-1/3"
+              >
+                <Trash size={16} /> Delete
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 }
